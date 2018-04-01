c51418fc0aac18b899f7ac2f235b6ae0302118bc
lwyj123 <443474713@qq.com>
2018-04-01 17:06:41 +0800

    modify pm2.js

--- pm2.js
+++ pm2.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [{
-    name: 'RESRful API Server',
-    script: './dist/app.js',
+    name: 'e-civ server',
+    script: './src/app.js',
     watch: false, // 默认关闭watch 可替换为 ['src']
     ignore_watch: ['node_modules', 'build', 'logs'],
-    out_file: '/logs/out.log', // 日志输出
-    error_file: '/logs/error.log', // 错误日志
+    out_file: './logs/out.log', // 日志输出
+    error_file: './logs/error.log', // 错误日志
     max_memory_restart: '2G', // 超过多大内存自动重启，仅防止内存泄露有意义，需要根据自己的业务设置
     env: {
       NODE_ENV: 'production'
