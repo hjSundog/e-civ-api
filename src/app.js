1666077b6a2801cad25ef629382887d5ad464c3b
lwyj123 <443474713@qq.com>
2018-04-06 14:35:57 +0800

    cors 2

--- src/app.js
+++ src/app.js
@@ -50,9 +50,9 @@ app
     //   ctx.set('Access-Control-Allow-Origin', SystemConfig.HTTP_server_host)
     // }
     // ctx.set('Access-Control-Allow-Origin', '*')
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
-    ctx.set('Access-Control-Allow-Credentials', true) // 允许带上 cookie
+    // ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    // ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+    // ctx.set('Access-Control-Allow-Credentials', true) // 允许带上 cookie
     return next()
   })
   .use(ErrorRoutesCatch())
