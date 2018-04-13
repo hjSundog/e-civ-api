8c8d57461961ec4ff472f2a094a791f04ed35ed2
breakinferno <1972952841@qq.com>
2018-04-13 13:04:36 +0800

    game remove

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,28 +1,28 @@
-const GameScene = require('e-civ-game')
+// const GameScene = require('e-civ-game')
 
 const Game = async (ctx) => {
-  let data
-  try {
-    if (typeof ctx.request.body === 'object') {
-      data = ctx.request.body
-    } else {
-      data = JSON.parse(ctx.request.body)
-    }
-  } catch (e) {
-    throw new Error(e)
-  }
+  // let data
+  // try {
+  //   if (typeof ctx.request.body === 'object') {
+  //     data = ctx.request.body
+  //   } else {
+  //     data = JSON.parse(ctx.request.body)
+  //   }
+  // } catch (e) {
+  //   throw new Error(e)
+  // }
 
-  const {own, enemy} = data
-  const game = new GameScene()
-  game.setClientOrServer(GameScene.SERVER)
-  game.makeScene('gameOver', () => {})
-  game.overScene('gameOver', null, null, (result, scene, bg) => {
-    ctx.body = bg.actionFlows
-  })
-  game.load(() => {
-    game.setSoilders(enemy, own)
-    game.start(own.user)
-  })
+  // const {own, enemy} = data
+  // const game = new GameScene()
+  // game.setClientOrServer(GameScene.SERVER)
+  // game.makeScene('gameOver', () => {})
+  // game.overScene('gameOver', null, null, (result, scene, bg) => {
+  //   ctx.body = bg.actionFlows
+  // })
+  // game.load(() => {
+  //   game.setSoilders(enemy, own)
+  //   game.start(own.user)
+  // })
 }
 
 module.exports = {
