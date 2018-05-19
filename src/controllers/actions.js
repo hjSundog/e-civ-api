d96f5312e88d757bcf38190caebde705ea379274
lwyj123 <443474713@qq.com>
2018-05-19 16:15:54 +0800

    feat: add cut action

--- src/controllers/actions.js
+++ src/controllers/actions.js
@@ -3,12 +3,12 @@ const Person = require('../models/person')
 const translator = require('../tpl/translator')
 const authInterceptor = require('../tool/Auth').authInterceptor
 const Tpls = translator(require('../tpl/ActionTpl'), 1)
+const schedule = require('node-schedule')
 const handleError = (err) => {
   console.log(err)
 }
 
 const doAction = async (ctx) => {
-  console.log('do action')
   if (!authInterceptor(ctx)) {
     return
   }
@@ -23,31 +23,47 @@ const doAction = async (ctx) => {
   const {id, target} = ctx.query
   const tpls = Tpls.get(type)
   console.log(tpls)
+
+  if (tpls.operation === 'walk') {
+    const user = ctx.header.authorization.decoded
+    const personDoc = await Person.findOne({user_id: user.id})
+    if (personDoc.current.toObject() !== 'idle') {
+      throw Error('当前人物不在空闲状态')
+    }
+    await Person.update({_id: personDoc._id}, {$set: { current: 'moving' }})
+    const targetPosition = {
+      // lon: ctx.query.lon,
+      // lat: ctx.query.lat
+      lon: 60,
+      lat: 24
+    }
+    const startPosition = personDoc.position
+
+    const error = {
+      lon: (targetPosition.lon - startPosition.lon) / 5,
+      lat: (targetPosition.lat - startPosition.lat) / 5
+    }
+    let count = 0
+
+    const job = schedule.scheduleJob('* /1 * * * *', function () {
+      console.log('position timer task')
+      count++
+      Person.update(
+        {_id: personDoc._id},
+        {
+          $set: {
+            'position.lon': startPosition.lon + error.lon * count,
+            'position.lat': startPosition.lat + error.lat * count
+
+          }
+        }
+      )
+      if (count === 5) {
+        job.cancel()
+      }
+    })
+  }
 }
-
-// const move = async (ctx) => {
-//   if (!authInterceptor(ctx)) {
-//     return
-//   }
-//   const user = ctx.header.authorization.decoded
-//   const personDoc = await Person.findOne({user_id: user.id})
-//   if (!ctx.params.lat || !ctx.params.lon) {
-//     ctx.response.status = 422
-//     ctx.body = {
-//       err: 'No Position Param'
-//     }
-//     return
-//   }
-//   if (personDoc.current.type !== 'idle') {
-//     ctx.response.status = 400
-//     ctx.body = {
-//       err: 'Person is not idle'
-//     }
-//   }
-//   const data = {}
-
-//   ctx.body = data
-// }
 
 module.exports = {
   doAction
